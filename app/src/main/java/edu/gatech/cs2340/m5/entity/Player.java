f4fb59f9f430e262b195e41ec88ee229ea3106eb
James Jiang <jjiang302@gatech.edu>
2019-02-17 18:06:12 -0500

    Added getters and setters to new player fields

--- app/src/main/java/edu/gatech/cs2340/m5/entity/Player.java
+++ app/src/main/java/edu/gatech/cs2340/m5/entity/Player.java
@@ -24,4 +24,48 @@ public class Player {
     public int getUserID() {
         return difficulty;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public void setPilotPoints(int pilotPoints) {
+        this.pilotPoints = pilotPoints;
+    }
+
+    public void setFighterPoints(int fighterPoints) {
+        this.fighterPoints = fighterPoints;
+    }
+
+    public void setTraderPoints(int traderPoints) {
+        this.traderPoints = traderPoints;
+    }
+
+    public void setEngineerPoints(int engineerPoints) {
+        this.engineerPoints = engineerPoints;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public int getPilotPoints() {
+        return pilotPoints;
+    }
+
+    public int getFighterPoints() {
+        return fighterPoints;
+    }
+
+    public int getTraderPoints() {
+        return traderPoints;
+    }
+
+    public int getEngineerPoints() {
+        return engineerPoints;
+    }
 }
