1b1af528ba6c081ecc27e7965b35a537b811001f
Jake Present <jake.present@codemettle.com>
2019-02-17 19:06:19 -0500

    Deleted ConfigurationActivity

--- app/src/main/java/edu/gatech/cs2340/m5/views/MainActivity.java
+++ app/src/main/java/edu/gatech/cs2340/m5/views/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import edu.gatech.cs2340.m5.R;
 
@@ -20,14 +22,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        Button doneButton = findViewById(R.id.newPlayerDoneButton);
+        Button difficultyDecrementButton = findViewById(R.id.difficultyDecrementButton);
+        Button difficultyIncrementButton = findViewById(R.id.difficultyIncrementButton);
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i("New Player", "new player");
             }
-        });*/
+        });
     }
 
     @Override
