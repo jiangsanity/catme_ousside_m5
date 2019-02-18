ad6d5dc94c67baad79a125ff2cce81a3faf64ea2
Jake Present <jake.present@codemettle.com>
2019-02-18 12:32:34 -0500

    Added AlertDialogs for approving or rejecting player completion

--- app/src/main/java/edu/gatech/cs2340/m5/views/MainActivity.java
+++ app/src/main/java/edu/gatech/cs2340/m5/views/MainActivity.java
@@ -1,8 +1,10 @@
 package edu.gatech.cs2340.m5.views;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -56,14 +58,39 @@ public class MainActivity extends AppCompatActivity {
 
         doneButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                player = new Player();
-                player.setName(nameField.getText().toString());
-                player.setDifficulty(difficulty);
-                player.setPilotPoints(Integer.parseInt(pilotView.getText().toString()));
-                player.setFighterPoints(Integer.parseInt(fighterView.getText().toString()));
-                player.setTraderPoints(Integer.parseInt(traderView.getText().toString()));
-                player.setEngineerPoints(Integer.parseInt(engineerView.getText().toString()));
-                Log.i("New Player", player.toString());
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this,
+                        R.style.Theme_MaterialComponents_Dialog_Alert);
+                if (pointsLeft == 0) {
+                    player = new Player();
+                    player.setName(nameField.getText().toString());
+                    player.setDifficulty(difficulty);
+                    player.setPilotPoints(Integer.parseInt(pilotView.getText().toString()));
+                    player.setFighterPoints(Integer.parseInt(fighterView.getText().toString()));
+                    player.setTraderPoints(Integer.parseInt(traderView.getText().toString()));
+                    player.setEngineerPoints(Integer.parseInt(engineerView.getText().toString()));
+                    Log.i("New Player", player.toString());
+                    builder.setTitle("Success")
+                            .setMessage("Player created:\n" + player.toString())
+                            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            })
+                            .show();
+                }
+                else {
+
+                    builder.setTitle("Invalid")
+                            .setMessage("All points not allocated")
+                            .setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            })
+                            .show();
+                }
             }
         });
 
