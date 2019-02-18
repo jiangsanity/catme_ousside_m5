634765e4908d436c8b3ff20820fe39a3dfb77c50
Fernanda Moreno <fmoreno6@gatech.edu>
2019-02-17 19:10:26 -0500

    added javadocs to Player class

--- app/src/main/java/edu/gatech/cs2340/m5/entity/Player.java
+++ app/src/main/java/edu/gatech/cs2340/m5/entity/Player.java
@@ -9,7 +9,16 @@ public class Player {
     private int engineerPoints;
     private String ship;
 
-
+    /**
+     * Constructs a new player
+     * @param name the name of the player
+     * @param difficulty the difficulty of the game
+     * @param pilotPoints number of points for pilot skill
+     * @param fighterPoints number of points for fighter skill
+     * @param traderPoints number of points for trader skill
+     * @param engineerPoints number of points for engineering skill
+     * @param ship the type of ship the player owns
+     */
     public Player(String name, int difficulty, int pilotPoints, int fighterPoints, int traderPoints,
                   int engineerPoints, String ship) {
         this.name = name;
@@ -26,61 +35,137 @@ public class Player {
                 -1, "Gnat");
     }
 
+    /**
+     *
+     * @return the name of the player
+     */
+
     public String getName() {
         return name;
     }
+
+    /**
+     *
+     * @return the chosen difficulty of the game
+     */
 
     public int getDifficulty() {
         return difficulty;
     }
 
+    /**
+     *
+     * @return the number of points for pilot skill
+     */
+
     public int getPilotPoints() {
         return pilotPoints;
     }
+
+    /**
+     *
+     * @return the number of points for fighter skill
+     */
 
     public int getFighterPoints() {
         return fighterPoints;
     }
 
+    /**
+     *
+     * @return the number of points for trader skill
+     */
+
     public int getTraderPoints() {
         return traderPoints;
     }
+
+    /**
+     *
+     * @return the number of points for engineering skill
+     */
 
     public int getEngineerPoints() {
         return engineerPoints;
     }
 
+    /**
+     *
+     * @return an array of the points for each skill
+     */
+
     public int[] getAllPoints() {
         return  new int[]{ pilotPoints, fighterPoints, traderPoints, engineerPoints};
     }
+
+    /**
+     *
+     * @return the type of ship the player owns
+     */
 
     public String getShip() {
         return ship;
     }
 
+    /**
+     *
+     * @param name the player's name
+     */
+
+
     public void setName(String name) {
         this.name = name;
     }
+
+    /**
+     *
+     * @param difficulty the game's difficulty
+     */
 
     public void setDifficulty(int difficulty) {
         this.difficulty = difficulty;
     }
 
+    /**
+     *
+     * @param pilotPoints points dedicated to pilot skill
+     */
+
     public void setPilotPoints(int pilotPoints) {
         this.pilotPoints = pilotPoints;
     }
+
+    /**
+     *
+     * @param fighterPoints points dedicated to fighter skill
+     */
 
     public void setFighterPoints(int fighterPoints) {
         this.fighterPoints = fighterPoints;
     }
 
+    /**
+     *
+     * @param traderPoints points dedicated to trader skill
+     */
+
     public void setTraderPoints(int traderPoints) {
         this.traderPoints = traderPoints;
     }
 
+    /**
+     * points dedicated to engineering skill
+     * @param engineerPoints
+     */
+
     public void setEngineerPoints(int engineerPoints) {
         this.engineerPoints = engineerPoints;
     }
+
+    /**
+     *
+     * @param ship type of ship the player owns
+     */
 
     public void setShip(String ship) {
         this.ship = ship;
